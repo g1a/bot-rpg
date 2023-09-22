6631b8b8e066176d86dce7bf86d9f50822d16afd
Greg Anderson <greg.1.anderson@greenknowe.org>
2023-09-21 18:52:30 -0700

    Non-working slash command example (close, but not quite).

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-import discord
 import os
 import pathlib
+
+import discord
+from discord import app_commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    #await tree.sync(guild=discord.Object(id='857097491131662346'))
+    await tree.sync(guild=discord.Object(id='857097491131662346'))
+    print("Ready!")
+
+@tree.command(description="Show Coat of Arms for a player or NPC", guild=discord.Object(id='857097491131662346'))
+@app_commands.describe(name='The player or NPC')
+async def arms(interaction: discord.Interaction):
+    name = interaction.data['options'][0]['value']
+    await interaction.response.send_message("Hello!")
+    armsPath = '{base}/Arms/{name}.png'.format(base=pathlib.Path(__file__).parent.resolve(), name=name)
+    await interaction.response.send_message('name is {name} and arms path is {path}'.format(name=name, path=armsPath))
+    await interaction.response.send_message(file=discord.File(armsPath))
 
 @client.event
 async def on_message(message):
