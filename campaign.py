c204b66fca667b3d44d4249c4ba0e514a5eaf020
Greg Anderson <greg.1.anderson@greenknowe.org>
2023-09-22 16:51:40 -0700

    Add simple date tracking commands

--- campaign.py
+++ campaign.py
@@ -1,14 +1,28 @@
 import os
 import os.path
 import pathlib
+import datetime
+from datetime import datetime
+from datetime import timedelta
+from pytimeparse.timeparse import timeparse
 
 from arms import Arms
 
 class Campaign:
-	def __init__(self, id):
+	def __init__(self, id, name):
 		self.id = id
+		self.name = name
 		self.base = '{project}/Campaigns/{id}'.format(project=pathlib.Path(__file__).parent.resolve(), id=self.id)
+		self.campaign_time = datetime.fromisoformat('1494-06-01 12:00:00')
 
 	def arms(self):
 		return Arms(self.base)
 
+	def set_time(self, time: str):
+		self.campaign_time = datetime.fromisoformat(time)
+
+	def time(self):
+		return self.campaign_time.strftime('%a %Y %b %d at %I:%M %p').replace(' 0', ' ')
+
+	def pass_time(self, delta: str):
+		self.campaign_time += timedelta(seconds=timeparse(delta))
--- main.py
+++ main.py
@@ -28,10 +28,13 @@ campaigns = {}
 
 def get_campaign(ctx):
     key = ctx.guild.id
+    name = ctx.guild.name
     # Future: some guilds might want separate campaigns per channel
     # channel=ctx.channel.name, cid=ctx.channel.id
+    # When we do this, we'll have to look up the campaign name and
+    # not just assume it is the same as the guild name.
     if not key in campaigns:
-        campaigns[key] = Campaign(key)
+        campaigns[key] = Campaign(key, name)
     return campaigns[key]
 
 bot = discord.Bot()
@@ -39,6 +42,25 @@ bot = discord.Bot()
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
+
+@bot.slash_command(name="set-time", description="Set the campaign time")
+@option("time", description="Campaign time")
+async def set_time(ctx, time: str):
+    campaign = get_campaign(ctx)
+    campaign.set_time(time)
+    await ctx.send_response('{campaign} time: {time}'.format(campaign=campaign.name, time=campaign.time()), ephemeral=True)
+
+@bot.slash_command(name="campaign-time", description="Display the current campaign time")
+async def campaign_time(ctx):
+    campaign = get_campaign(ctx)
+    await ctx.send_response('{campaign} time: {time}'.format(campaign=campaign.name, time=campaign.time()))
+
+@bot.slash_command(name="pass-time", description="Advance the campaign time when time passes")
+@option("delta", description="How much time has passed")
+async def pass_time(ctx, delta: str):
+    campaign = get_campaign(ctx)
+    campaign.pass_time(delta)
+    await ctx.send_response('{campaign} time: {time}'.format(campaign=campaign.name, time=campaign.time()), ephemeral=True)
 
 @bot.slash_command(name="arms", description="Show Coat of Arms for a player or NPC", guild_ids=['857097491131662346'])
 @option("name", description="Player or NPC")
@@ -52,4 +74,5 @@ async def arms(ctx, name: str):
     await ctx.respond('Guild: {guild} ({id}). Channel: {channel} ({cid}). Arms for {name}:'.format(guild=ctx.guild.name, id=ctx.guild.id, channel=ctx.channel.name, cid=ctx.channel.id, name=name), ephemeral=True)
 
 #client.run(os.getenv('TOKEN'))
-bot.run(os.getenv('TOKEN'))
+if __name__ == "__main__":
+    bot.run(os.getenv('TOKEN'))
